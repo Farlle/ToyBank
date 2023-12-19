d5711d64a6c125e0355e23cbc7eeb47446e92160
Farlle <zh.alexander.qq@gmail.com>
2023-12-19 15:02:43 +0300

    updateBackSystem

--- src/com/company/backendsystem/BackendSystem.java
+++ src/com/company/backendsystem/BackendSystem.java
@@ -3,12 +3,14 @@ package com.company.backendsystem;
 import com.company.application.Application;
 import com.company.application.Operation;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class BackendSystem {
 
-    private long balance;
+    private AtomicLong balance = new AtomicLong();
 
     public BackendSystem(long balance) {
-        this.balance = balance;
+        this.balance.getAndUpdate((x) -> balance + x);
     }
 
 
@@ -27,7 +29,7 @@ public class BackendSystem {
     }
 
     public synchronized void decreaseBalance(Application application, String nameApplication) {
-        balance += application.getSum();
+        balance.getAndUpdate((x) -> x + application.getSum());
         System.out.printf("БЭК система: Заявка %s от %s обработана. Баланс банка = %d\n",
                 application, nameApplication, balance);
     }
@@ -35,12 +37,12 @@ public class BackendSystem {
 
     public synchronized void increaseBalance(Application application, String nameApplication) {
         long amount = application.getSum();
-        while (balance - amount < 0) {
+        while (balance.get() > amount) {
             System.out.printf("БЭК система: Заявка %s от %s не обработана. Баланс банка = %d\n",
                     application, nameApplication, balance);
             return;
         }
-        balance -= amount;
+        balance.getAndUpdate((x) -> x - application.getSum());
         System.out.printf("БЭК система: Заявка %s от %s обработана. Баланс банка = %d\n",
                 application, nameApplication, balance);
     }
